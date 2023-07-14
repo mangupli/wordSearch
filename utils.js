91c6cdf7aaaf9e87143aa89ef8a699229c43fa33
mangupli <elizaveta.mangupli@gmail.com>
2023-07-14 15:07:28 +0300

    function for word searching

---
+++ utils.js
@@ -0,0 +1,53 @@
+const notFound = 'Слово не найдено';
+
+const steps = {
+  diagonal: [
+    [-1, -1],
+    [1, 1],
+    [-1, 1],
+    [1, -1],
+  ],
+  straight: [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ],
+  all: [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+    [-1, -1],
+    [1, 1],
+    [-1, 1],
+    [1, -1],
+  ],
+};
+
+function getNeighbors({ x, y }, puzzle, directions) {
+  const width = puzzle[0].length;
+  const height = puzzle.length;
+
+  const neighbors = [];
+
+  directions.forEach((direction) => {
+    const neighbor = [x + direction[0], y + direction[1]];
+    if (
+      neighbor[0] < height &&
+      neighbor[0] >= 0 &&
+      neighbor[1] < width &&
+      neighbor[1] >= 0
+    ) {
+      neighbors.push(neighbor);
+    }
+  });
+
+  return neighbors;
+}
+
+module.exports = {
+  notFound,
+  getNeighbors,
+  steps,
+};
---
+++ wordSearch.js
@@ -0,0 +1,62 @@
+const { getNeighbors, notFound, steps } = require('./utils');
+
+const testPuzzle = [
+  ['a', 'a', 'a'],
+  ['b', 'b', 'b'],
+];
+
+function dfsUtil(coord, word, nextPosition, puzzle, visited, options) {
+  // if all the letters has been found previously
+  if (nextPosition === word.length) return true;
+
+  // eslint-disable-next-line no-param-reassign
+  visited[`${coord.x}${coord.y}`] = true;
+
+  //
+  const nearest = getNeighbors(coord, puzzle, steps[options.search]);
+
+  for (let i = 0; i < nearest.length; i++) {
+    const x = nearest[i][0];
+    const y = nearest[i][1];
+
+    if (!visited[`${x}${y}`] && puzzle[x][y] === word[nextPosition]) {
+      return dfsUtil(
+        { x, y },
+        word,
+        nextPosition + 1,
+        puzzle,
+        visited,
+        options
+      );
+    }
+  }
+  return false;
+}
+
+function dfs(coord, word, puzzle, options) {
+  // there will be all the visited cells by dfs algo
+  const visited = {};
+  const nextLetterIndex = 1;
+
+  return dfsUtil(coord, word, nextLetterIndex, puzzle, visited, options);
+}
+
+// search =  'diagonal' | 'straight' | 'all'
+
+function wordSearch(word, puzzle, options = { search: 'straight' }) {
+  const firstLetter = word[0];
+
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle[i].length; j++) {
+      if (puzzle[i][j] === firstLetter) {
+        const coord = { x: i, y: j };
+        const found = dfs(coord, word, puzzle, options);
+        if (found) return [i, j];
+      }
+    }
+  }
+  return notFound;
+}
+
+
+module.exports = wordSearch;
